7899418fc9a99206e66ed6973ae8ec8e48db4104
Everlon Passos <dev@everlon.com.br>
2024-06-20 12:15:49 -0300

    Rotas 4 e 5

--- api/app.py
+++ api/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
 from markupsafe import escape
 
-import functions as Func
+from functions import BashManager
 
 
 app = Flask(__name__)
-
+bash_manager = BashManager()
 
 # Exemplo de Methods e já retorna Json por default.
 @app.route('/', methods=['GET', 'POST'])
@@ -31,46 +31,70 @@ def show_user_profile(username):
     return f'User {escape(username)}', 200
 
 
-# Exemplo de executar BASH.
-@app.route('/executar-script', methods=['GET'])
-def executar_script():
-    try:
-        arquivo = "../data/raw/input"
-        limite_inferior = "50"
-        limite_superior = "200"
-        bash_output = Func.exec_script_bash('./../scripts/bash/between-msgs.sh', arquivo, limite_inferior, limite_superior)
-        app.logger.info('Script BASH (between-msgs.sh) executado com sucesso!')
-
-        return jsonify(Func.convert_to_json_format(bash_output)), 200
-
-    except Exception as e:
-        app.logger.error('An error occurred: (%s)', e)
-        return f"Erro: {e}", 500
-
-
-# ROUTE usuários ordenados.
-# /users?order=asc&limit=15&page=1&filter=example
+# ROUTE 4: Usuários.
+# /users?file=input&order=asc&page=1&limit=15&filter=example
 @app.route('/users', methods=['GET'])
 def exec_script_order_username():
     try:
-        arquivo = "../data/raw/input"
+        arquivo = request.args.get('file', default='', type=str)
+        file = bash_manager.search_file(arquivo)
+
+        if not arquivo or not file:
+            return {"Error": "Arquivo informado não existe."}, 404
+
         order = request.args.get('order', default='asc', type=str)
-        limit = request.args.get('limit')
+        limit = request.args.get('limit', default=0)
         page = request.args.get('page', default=1, type=int)
         filter_term = request.args.get('filter', default='')
 
-        bash_output = Func.exec_script_bash('./../scripts/bash/order-by-username.sh', arquivo, order)
+        bash_output = bash_manager.exec_script_bash('./../scripts/bash/order-by-username.sh', f"../data/raw/{file[0]}", order)
         app.logger.info('Script BASH (order-by-username.sh) executado com sucesso!')
 
-        users = Func.convert_to_json_format(bash_output)
-        users = Func.filter_users(users, filter_term)
-        users = Func.paginate_users(users, page, limit)
+        users = bash_manager.convert_to_json_format(bash_output)
+        users = bash_manager.filter_users(users, filter_term)
+        users = bash_manager.paginate_users(users, page, limit)
 
         return jsonify(users), 200
 
     except Exception as e:
         app.logger.error('An error occurred: (%s)', e)
-        return f"Erro: {e}", 500
+        return f"Error: {e}", 500
+
+
+# ROUTE 5: Quantidade de mensagens na INBOX.
+# /users-between?file=input&min=50&max=200&page=1&filter=example
+@app.route('/users-between', methods=['GET'])
+def executar_script():
+    try:
+        arquivo = request.args.get('file', default='', type=str)
+        file = bash_manager.search_file(arquivo)
+
+        if not arquivo or not file:
+            return {"Error": "Arquivo informado não existe."}, 404
+
+        limit_min = request.args.get('min')
+        limit_max = request.args.get('max')
+
+        if not limit_min and not limit_max:
+            return {"Error": "Informar valor mínimo (min=) e máximo (max=) para faixa de quantidade de mensagens."}, 400
+
+        page = request.args.get('page', default=1, type=int)
+        limit = request.args.get('limit', default=0)
+        filter_term = request.args.get('filter', default='')
+
+        bash_output = bash_manager.exec_script_bash('./../scripts/bash/between-msgs.sh', f"../data/raw/{file[0]}", limit_min, limit_max)
+        app.logger.info('Script BASH (between-msgs.sh) executado com sucesso!')
+
+        users = bash_manager.convert_to_json_format(bash_output)
+        users = bash_manager.filter_users(users, filter_term)
+        users = bash_manager.paginate_users(users, page, limit)
+
+        return jsonify(users), 200
+
+    except Exception as e:
+        app.logger.error('An error occurred: (%s)', e)
+        return f"Error: {e}", 500
+
 
 
 # Exemplo de retorno com número de Erros em JSON.
@@ -86,4 +110,4 @@ if __name__ == '__main__':
 
 
 # Rodar aplicação flask.
-# flask --app app run
+# flask --app app run --debug
--- api/functions.py
+++ api/functions.py
@@ -1,54 +1,75 @@
 import subprocess
 import re
+import os
 # import json
 
-def exec_script_bash(script_path, *args):
-    # Função para executar BASH.
-    # TODO: Separar em arquivos diferente.
 
-    result = subprocess.run([script_path, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    if result.returncode != 0:
-        raise Exception(f"Erro ao executar o script Bash: {result.stderr}")
-    return result.stdout
+class BashManager:
+    def __init__(self, data_dir='../data/raw'):
+        self.data_dir = data_dir
 
-def convert_to_json_format(bash_output):
-    linhas = bash_output.strip().split('\n')
-    users = []
-    for linha in linhas:
-        match = re.match(r'(\S+@\S+) (\S+) (\d+) size (\d+)', linha)
-        if match:
-            user = {
-                "username": match.group(1),
-                "folder": match.group(2),
-                "numberMessages": int(match.group(3)),
-                "size": int(match.group(4))
-            }
-            users.append(user)
+    def exec_script_bash(self, script_path, *args):
+        # Função para executar BASH.
 
-    return users
-    # return json.dumps(users, indent=2)
+        result = subprocess.run([script_path, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if result.returncode != 0:
+            raise Exception(f"Erro ao executar o script Bash: {result.stderr}")
+        return result.stdout
 
-def filter_users(users, filter_term):
-    if filter_term:
-        users = [user for user in users if filter_term.lower() in user['username'].lower()]
-    return users
+    def convert_to_json_format(self, bash_output):
+        linhas = bash_output.strip().split('\n')
+        users = []
+        for linha in linhas:
+            match = re.match(r'(\S+@\S+) (\S+) (\d+) size (\d+)', linha)
+            if match:
+                user = {
+                    "username": match.group(1),
+                    "folder": match.group(2),
+                    "numberMessages": int(match.group(3)),
+                    "size": int(match.group(4))
+                }
+                users.append(user)
 
-def paginate_users(users, page, limit):
-    # Paginação: Limit para limitar quantidade por página e Page como número de página
+        return users
+        # return json.dumps(users, indent=2)
 
-    page = int(page)
-    limit = int(limit)
-
-    if limit <= 0:
+    def filter_users(self, users, filter_term):
+        if filter_term:
+            users = [user for user in users if filter_term.lower() in user['username'].lower()]
         return users
 
-    start = (page - 1) * limit
-    end = start + limit
+    def search_file(self, search=None):
+        # Procurar arquivo na pasta "../data/raw". Se não informar search irá listar todos.
+        try:
+            files = os.listdir(self.data_dir)
 
-    if start >= len(users):
-        return []
+            if search:
+                # Selecionar o arquivo de nome idêntico ao informado.
+                files = [f for f in files if f == search and os.path.isfile(os.path.join(self.data_dir, f))]
+            else:
+                files = [f for f in files if os.path.isfile(os.path.join(self.data_dir, f))]
 
-    if end > len(users):
-        end = len(users)
+            return files
 
-    return users[start:end]
+        except FileNotFoundError:
+            return []
+
+    def paginate_users(self, users, page, limit):
+        # Paginação: Limit para limitar quantidade por página e Page como número de página
+
+        page = int(page)
+        limit = int(limit)
+
+        if limit <= 0:
+            return users
+
+        start = (page - 1) * limit
+        end = start + limit
+
+        if start >= len(users):
+            return []
+
+        if end > len(users):
+            end = len(users)
+
+        return users[start:end]
